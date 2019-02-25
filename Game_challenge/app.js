a5b7bf5def5e1f1b0a4a6d440f0b60ac2cd06c7b
Luis Javier Cabello de Alba Fernandez <luisjavier.cabellodealba@bbva.com>
2019-02-25 07:59:42 +0100

    Attack function created

--- Game_challenge/app.js
+++ Game_challenge/app.js
@@ -8,8 +8,39 @@ new Vue({
   methods: {
     startGame: function() {
       this.gameIsRunning = true;
-      this.playerHealth: 100;
-      this.monsterHealth: 100;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack: function(params) {
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert('You won!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0) {
+        alert('You lost!');
+        this.gameIsRunning = false;
+      }
+    },
+    specialAttack: function(params) {
+
+    },
+    heal: function(params) {
+
+    },
+    giveUp: function(params) {
     }
   },
   computed: {
